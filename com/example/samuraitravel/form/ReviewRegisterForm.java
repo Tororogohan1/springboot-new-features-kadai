eb7ad8fd104c508ebdcc6dc9f63c1a6e2824c6d5
Tororogohan1 <ri1003sen@docomo.ne.jp>
2025-04-13 18:28:10 +0900

    Add files via upload

---
+++ com/example/samuraitravel/form/ReviewRegisterForm.java
@@ -0,0 +1,29 @@
+package com.example.samuraitravel.form;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReviewRegisterForm {
+
+    @NotNull
+    private Integer houseId;
+
+    @NotNull
+    private Integer userId;
+
+    @NotNull(message = "評価を選択してください。")
+    @Min(value = 1, message = "1〜5の間で選択してください。")
+    @Max(value = 5, message = "1〜5の間で選択してください。")
+    private String reviewStar;
+
+    @NotBlank(message = "コメントを入力してください。")
+    private String reviewComment;
+}
